c14949ada6eb3b8b374db58a2be89c8a8b7c5802
Luis Felipe Pereira <luis.lipebr321@gmail.com>
2023-10-08 15:53:32 -0300

    api e banco na nuvem

--- API/APIPetFeliz/DTO/EspecieDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace APIPetFeliz.DTO
-{
-    public class EspecieDTO
-    {
-        public int Id_Especie { get; set; }
-        public string Nome_Especie { get; set;}
-    }
-}
--- API/APIPetFeliz/DTO/RacaDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace APIPetFeliz.DTO
-{
-    public class RacaDTO
-    {
-        public int Id_Raca { get; set; }
-        public string Nome_Raca { get; set; }
-        public int Cod_Especie { get; set; }
-    }
-}
