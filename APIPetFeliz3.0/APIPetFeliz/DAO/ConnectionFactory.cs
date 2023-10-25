3e767f88f7b5da8916d8cb506871faf19aae7cbf
Luis Felipe Pereira <luis.lipebr321@gmail.com>
2023-10-25 18:17:56 -0300

    adcionando a api e bando dos teste para previa

---
+++ APIPetFeliz3.0/APIPetFeliz/DAO/ConnectionFactory.cs
@@ -0,0 +1,12 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace APIPetFeliz.DAO
+{
+    public class ConnectionFactory
+    {
+        public static MySqlConnection Build()
+        {
+            return new MySqlConnection("Server=localhost;Database=Bd_Pet_Feliz;Uid=root;Pwd=root;");
+        }
+    }
+}
